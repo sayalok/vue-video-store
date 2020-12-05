cbf3641733afd3e7a6c7ba8d74fca615348cdab6
me <sayalok@yahoo.com>
2020-12-06 01:06:28 +0600

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<SearchBar/>
+	</div>
+</template>
+
+<script>
+	import SearchBar from './components/SearchBar'
+	export default {
+		name: 'App',
+		components: {
+			SearchBar
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="text" @input="onInput">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBar',
+        methods: {
+            onInput: function(e) {
+                console.log(e.target.value)
+                
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
